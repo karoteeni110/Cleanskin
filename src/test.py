46b25ff6017265c114942109bbc95e07b00d1906
karoteeni110 <akb48auy@sohu.com>
2019-06-05 17:21:33 +0300

    abstract & intro works; other sections problematic

--- src/test.py
+++ src/test.py
@@ -8,12 +8,20 @@ with open(tester_tex_path, mode='r', encoding='utf-8') as tex_file:
     data = tex_file.read()
 
 # == Paterns ==
-abstract_pt = r'\\begin{abstract}(.*?)\\end{abstract}'
-intro_pt = r'\\section{[iI]ntroduction}(.*?)\\section{'
-
+abstract_pt = r'(\\begin{abstract}.*?\\end{abstract})'
+intro_pt = r'(\\section{[iI]ntroduction}.*?\\section{)'
+othersec_pt = r'((\\section{.*?)(\\section|\n\n))'
+patterns = [abstract_pt, intro_pt, othersec_pt]
 
 # == Output == 
-section = re.findall(abstract_pt, data, re.S)
-print(section)
-# with open(os.path.join(results_dir, '/extracted.txt'),'w') as out:
-#     out.write(section) 
+sections = []
+for pt in patterns:
+    sec = re.findall(pt, data, re.S)
+    if len(sec)>1:
+        print(sec)
+    sections.extend(sec)
+exit(0)
+with open(os.path.join(results_dir, 'out.txt'),'w') as out:
+    print(sections)
+    extracted = ' '.join(sections)
+    out.write(extracted) 
