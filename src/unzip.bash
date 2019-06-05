fd7f3365cdb80f80116a04646e2fb8d8fd5ef125
karoteeni110 <akb48auy@sohu.com>
2019-06-05 13:00:48 +0300

    unzip pipeline completed

--- src/unzip.bash
+++ src/unzip.bash
@@ -1,2 +1,12 @@
-gunzip astro-ph0001001.tar.gz
-tar -xvf astro-ph0001001 --one-top-level=astro-ph0001001
+#gunzip astro-*.gz
+
+for basename in *0001* ; do # untar
+  tar -xvf "$basename" --one-top-level=="$basename" 
+  if [[ ! $basename =~ "=" ]] ; then # if it's not a directory, move to /cluster
+    mv $basename cluster
+  fi
+done
+
+# word="tiger"
+# firstletter=${word:0:1}
+# echo $firstletter
