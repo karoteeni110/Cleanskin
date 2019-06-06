28b975e21a2d6b8434ad984a3e1b2ad98ab81a96
karoteeni110 <akb48auy@sohu.com>
2019-06-06 17:09:56 +0300

    Notes; sec pattern; tabular pt

--- src/test.py
+++ src/test.py
@@ -1,5 +1,4 @@
-from reader import *
-from paths import *
+from paths import tester_tex_path, results_dir
 import re, os
 
 # == Loading data ==
@@ -15,14 +14,30 @@ ref = '\\ref{.*?}'
 label = '\\label{.*?}'
 comments = '^%.*?$'
 equationpt = '\\begin{equation}.*?\\end{equation}'
+tabular = '\\begin{tabular}.*?\\end{tabular}'
+#items = '\\begin{itemize}.*?\\end{itemize}' # Nested items???
 mathmodept = '(\${.*?}\$|\$\$.*?\$\$)'
 macros = '(%s|%s|%s|%s|%s|%s)' % (citept, ref, label, comments, equationpt, mathmodept)
 
 # To be captured:
+"""
+Notes: 
+To generalize the pattern, maybe peek first what is the command for introduction.
+
+Known patterns:
+1. ABSTRACT
+\abstract{Emission in spectral lines can provide unique information...}
+\begin{abstract} ... \end{abstract}
+
+2. Intro / other secs
+\chapter{Introduction}
+\section{Introduction}
+
+"""
 abstract_pt = r'(\\begin{abstract}.*\\end{abstract})' # we need the full match
 #middle_secs_pt = r'((\\section{.*?)(^\\section))' # only group 1 needed
-last_sec_pt = r'(\\section{.*?)(\\begin{thebibliography})'
-patterns = [abstract_pt, last_sec_pt]
+secs_pt = r'(\\section{.*?)(\\begin{thebibliography})' # TODO: generalizable?
+patterns = [abstract_pt, secs_pt]
 
 # == Filtering macros == #
 # == Extracting == 
