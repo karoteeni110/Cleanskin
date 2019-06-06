2e152b2a26f28f2c53cc9791c280a300f0d4aca9
karoteeni110 <akb48auy@sohu.com>
2019-06-06 12:37:15 +0300

    SMI flags for re.finditer()

--- src/test.py
+++ src/test.py
@@ -10,20 +10,21 @@ with open(tester_tex_path, mode='r', encoding='utf-8') as tex_file:
 # == Paterns ==
 abstract_pt = r'(\\begin{abstract}.*\\end{abstract})' # we need the full match
 intro_pt = r'(\\section{[iI]ntroduction}.*)(\\section{)' # only group 1 needed
-othersecs_pt = r'(\\section{.*)(\\section|\n\n)' # only group 1 needed
-patterns = [abstract_pt, intro_pt, othersecs_pt]
+othersecs_pt = r'(\\section{.*)(^\\)' # only group 1 needed
+patterns = [ abstract_pt, othersecs_pt]
 
 # == Extracting == 
 extracted = ''
 for idx, pt in enumerate(patterns):
-    for match in re.finditer(pt, data, re.S):
+    for match in re.finditer(pt, data, re.S | re.M | re.I):
         if idx == 0:  
-            grp = 0 # fetch full match for abstract section
+            grp = 0 # full match for abstract section
         else:
-            grp = 1 # group 1 for other sections
+            grp = 0 # group 1 for the other sections
         body = match.group(grp) 
+        print(idx, body[:1000])
     extracted += '\n\n' + body
-
+    
 # == Output ==
 with open(os.path.join(results_dir, 'out.txt'),'w') as out:
     out.write(extracted) 
