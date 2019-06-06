5f798743c0da4c766589da974d8753c411ea7589
karoteeni110 <akb48auy@sohu.com>
2019-06-06 14:10:31 +0300

    Patterns fixed; match.groups()[0]

--- src/test.py
+++ src/test.py
@@ -9,22 +9,17 @@ with open(tester_tex_path, mode='r', encoding='utf-8') as tex_file:
 
 # == Paterns ==
 abstract_pt = r'(\\begin{abstract}.*\\end{abstract})' # we need the full match
-intro_pt = r'(\\section{[iI]ntroduction}.*)(\\section{)' # only group 1 needed
-othersecs_pt = r'(\\section{.*)(^\\)' # only group 1 needed
-patterns = [ abstract_pt, othersecs_pt]
+#middle_secs_pt = r'(\\section{.*)(\\section{)' # only group 1 needed
+secs_pt = r'(\\section{.*?)(^\\)' # only group 1 needed
+patterns = [abstract_pt, secs_pt]
 
 # == Extracting == 
 extracted = ''
 for idx, pt in enumerate(patterns):
     for match in re.finditer(pt, data, re.S | re.M | re.I):
-        if idx == 0:  
-            grp = 0 # full match for abstract section
-        else:
-            grp = 0 # group 1 for the other sections
-        body = match.group(grp) 
-        print(idx, body[:1000])
-    extracted += '\n\n' + body
-    
+        body = match.groups()[0] # the full match for abstract OR the group 1 for other secs
+        extracted += '\n\n' + body
+
 # == Output ==
 with open(os.path.join(results_dir, 'out.txt'),'w') as out:
     out.write(extracted) 
