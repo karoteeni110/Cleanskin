90279d37a9869858ffe5da400de66d8be6944df7
karoteeni110 <akb48auy@sohu.com>
2019-06-07 11:00:24 +0300

    checked bibliography pattern

--- src/checkpattern.sh
+++ src/checkpattern.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-grep '[Ii]ntroduction' ./*/*.tex > intro.txt
-grep 'abstract' ./*/*.tex > abstract.txt
+# grep '[Ii]ntroduction' ./*/*.tex > intro.txt
+grep 'thebibliography' ./*/*.tex > bib.txt
