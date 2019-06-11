0fa11830bdae0f2fdbd5fb1a938c0f652e706292
karoteeni110 <akb48auy@sohu.com>
2019-06-11 19:56:55 +0300

    "meta as dictionary" check

--- src/pandocFilter.py
+++ src/pandocFilter.py
@@ -1,21 +1,35 @@
 """
-Pandoc filter that makes convertion ignore comments,
-math mode, images and tables.
+A pandoc filter that aims to:
+    Turn the abstracts in metadata into paragraphs;  
+    Ignore comments, math mode, images and tables.
 
 Note: The return of an action is either:
-
     None: this means that the object should remain unchanged
     a pandoc object: this will replace the original object
     a list of pandoc objects: these will replace the original object; the list is merged with the neighbors of the original objects (spliced into the list the original object belongs to); returning an empty list deletes the object
 
 """
 
-from pandocfilters import toJSONFilter, Math, Para, Emph
+from pandocfilters import toJSONFilters, Math, Para, Emph
 import re
 
+def meta2para(k, v, fmt, meta):
+  return None
+
 def no_math(k, v, fmt, meta):
-  if k =='Math':
-    return Math()
+  return []
+
+def no_comment(k, v, fmt, meta):
+  return []
+
+def no_table(k, v, fmt, meta):
+  return []
+
+def no_image(k, v, fmt, meta):
+  return []
+
+
+actions = [no_math, no_comment, no_table, no_image]
 
 if __name__ == "__main__":
-  toJSONFilter(no_math)
+  toJSONFilters(actions)
