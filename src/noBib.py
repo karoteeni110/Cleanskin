2390af7a302d6c46810e75d8dd8aa953febcdcef
karoteeni110 <akb48auy@sohu.com>
2019-06-12 19:23:49 +0300

    TODO: filenames

--- src/noBib.py
+++ src/noBib.py
@@ -1,21 +1,51 @@
 """
 Cleans {thebibliography} section before using Pandoc. 
+
+Usage: 
+    For test (debib a single tex file in /data):
+        python3 noBib.py TEX_FILE_NAME 
+    For all the files in /data:
+        python3 noBib.py GOGOGO
 """
 import re, os, sys
 from paths import results_dir, data_path
 
-# Read data
-f = sys.argv[1]
-fpath = os.path.join(data_path, f)
-with open(fpath, mode='r', encoding='utf-8') as texfile:
-    data = texfile.read()
+VERBOSE = True
+REPORT_EVERY = 5
+def list_input(arg):
+    if arg != 'GOGOGO':
+        all_files = [ os.path.join(data_path, arg) ]
+    else:
+        all_files = []
+        for root, _, folderfiles in os.walk(data_path):
+            for singlefile in folderfiles:
+                if '.tex' in singlefile:
+                    all_files.append(os.path.join(root, singlefile))
+    return all_files
 
-# Patterns
-bib = r'(\\begin{thebibliography}.*?\\end{thebibliography})'
+def main():
+    # Read data
+    f = sys.argv[1]
+    all_files = list_input(f)
 
-# Substitute & write out
-print('Stripping bibliography...')
-clean_data = re.sub(bib,'', data, flags=re.S | re.M | re.I)
+    # Pattern
+    bib = r'(\\begin{thebibliography}.*?\\end{thebibliography})'
+    
+    # Substitute & write out
+    for fpath in all_files:
+        with open(fpath, mode='r', encoding='utf-8') as texfile:
+            data = texfile.read()
 
-with open(os.path.join(results_dir, '%s_debib.tex' % f[:-4]),'w') as out:
-    out.write(clean_data)
+        if VERBOSE:
+            if len(all_files)==1:
+                REPORT_EVERY = 1
+            for i in range(1,len(all_files), REPORT_EVERY):
+                print('Stripping file %d: %s' % (i, f))
+
+        clean_data = re.sub(bib,'', data, flags=re.S | re.M | re.I)
+
+        with open(os.path.join(results_dir, '%s_debib.tex' % f[:-4]),'w') as out:
+            out.write(clean_data)
+            
+if __name__ == "__main__":
+    main()
