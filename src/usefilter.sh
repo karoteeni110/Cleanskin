95728be75240f8e456c6f6967e604e32da20fe8e
karoteeni110 <akb48auy@sohu.com>
2019-06-12 17:44:18 +0300

    template working!

--- src/usefilter.sh
+++ src/usefilter.sh
@@ -1,4 +1,5 @@
 # pandoc ../data/nat_orders_revisionv4Amaro.tex -f latex -t html -s -o non-filtered.html
-pandoc ../data/nat_orders_revisionv4Amaro.tex -f latex -t html --filter pandocFilter.py -s -o filtered.html
+pandoc ../data/nat_orders_revisionv4Amaro.tex -f latex -t opml --filter pandocFilter.py \
+--template=/home/local/yzan/Desktop/Cleanskin/data/temp.xml -s -o filtered.xml
 # pandoc -s ../data/nat_orders_revisionv4Amaro.tex -o paper.html
 # pandoc -s -t JSON ../data/nat_orders_revisionv4Amaro.tex > pandoAST.txt # Check the AST of the file
