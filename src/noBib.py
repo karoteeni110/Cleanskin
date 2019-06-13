bad0a735f891e03e807187b0cd4be6997074d0a6
karoteeni110 <akb48auy@sohu.com>
2019-06-13 11:41:34 +0300

    verbose, filename fixed

--- src/noBib.py
+++ src/noBib.py
@@ -12,39 +12,43 @@ from paths import results_dir, data_path
 
 VERBOSE = True
 REPORT_EVERY = 5
+
 def list_input(arg):
+    """
+    Return a list of (path, filename) for each of the .tex.
+    """
     if arg != 'GOGOGO':
-        all_files = [ os.path.join(data_path, arg) ]
+        all_files = [ (os.path.join(data_path, arg), arg) ]
     else:
         all_files = []
         for root, _, folderfiles in os.walk(data_path):
             for singlefile in folderfiles:
                 if '.tex' in singlefile:
-                    all_files.append(os.path.join(root, singlefile))
+                    all_files.append((os.path.join(root, singlefile), singlefile))
     return all_files
 
 def main():
     # Read data
-    f = sys.argv[1]
-    all_files = list_input(f)
+    all_files = list_input(sys.argv[1])
 
     # Pattern
     bib = r'(\\begin{thebibliography}.*?\\end{thebibliography})'
     
     # Substitute & write out
-    for fpath in all_files:
+    for i, (fpath, fname) in enumerate(all_files):
         with open(fpath, mode='r', encoding='utf-8') as texfile:
             data = texfile.read()
 
         if VERBOSE:
-            if len(all_files)==1:
-                REPORT_EVERY = 1
-            for i in range(1,len(all_files), REPORT_EVERY):
-                print('Stripping file %d: %s' % (i, f))
+            if len(all_files) == 1:
+                every = 1
+            else:
+                every = REPORT_EVERY        
+            if i % every == 0 or i == len(all_files)-1: 
+                print('Stripping file %d: %s' % (i+1, fname))
 
         clean_data = re.sub(bib,'', data, flags=re.S | re.M | re.I)
-
-        with open(os.path.join(results_dir, '%s_debib.tex' % f[:-4]),'w') as out:
+        with open(os.path.join(results_dir, '%s_debib.tex' % fname[:-4]),'w') as out:
             out.write(clean_data)
             
 if __name__ == "__main__":
