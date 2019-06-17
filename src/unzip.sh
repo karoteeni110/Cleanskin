5f2f3a1b7e514ee99e7311e7da1f36eb9f6c650b
karoteeni110 <akb48auy@sohu.com>
2019-06-17 18:04:42 +0300

    unzip bug fixed

--- src/unzip.sh
+++ src/unzip.sh
@@ -1,16 +1,17 @@
 # !/bin/bash
 
-if cd ../data/0001; then
-  gunzip *0001*.gz
+if cd ../data/0002; then
+  gunzip *0002*.gz
 else
   echo 'Could not change directory! Aborting.' 1>&2
   exit 1
 fi
 
-for basename in *0001* ; do # untar
+for basename in *0002* ; do # untar
   if [ ${basename: -4} != ".pdf" ] || [ $basename != cluster*]; then 
     tar -xvf "$basename" --one-top-level=="$basename" 
-  else # if not a directory, move to /cluster
+  fi
+  if [[ ! $basename =~ "=" ]] ; then # if not a directory, move to /cluster
     mv $basename ./cluster/
   fi
 done
--- src/usefilter.sh
+++ src/usefilter.sh
@@ -1,12 +1,12 @@
 # pandoc ../data/nat_orders_revisionv4Amaro.tex -f latex -t html -s -o non-filtered.html
 # cwd: /src/
-if cd ../results/0001; then
+if cd ../results/0002; then
     # set -e # Stop script if a simple command fails
-    for basename in =*0001*_debib.tex; do
+    for basename in =*0002*_debib.tex; do
         pandoc $basename -f latex -t opml \
         --filter /home/local/yzan/Desktop/Cleanskin/src/pandocFilter.py \
         --template=/home/local/yzan/Desktop/Cleanskin/data/temp.xml \
-        -s -o 0001filtered/${basename:0:-10}_filtered.xml
+        -s -o 0002filtered/${basename:0:-10}_filtered.xml
     done
 fi
 
