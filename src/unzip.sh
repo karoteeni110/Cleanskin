614523219c11939f6dfad8c602e786cf45909c65
karoteeni110 <akb48auy@sohu.com>
2019-06-17 17:49:53 +0300

    if cd; xml name; debib name

--- src/unzip.sh
+++ src/unzip.sh
@@ -1,13 +1,17 @@
 # !/bin/bash
 
-gunzip 1701.*.gz
+if cd ../data/0001; then
+  gunzip *0001*.gz
+else
+  echo 'Could not change directory! Aborting.' 1>&2
+  exit 1
+fi
 
-for basename in 1701.* ; do # untar
-  if [ ${basename: -4} != ".pdf" ] || [ $basename = cluster*]; then 
+for basename in *0001* ; do # untar
+  if [ ${basename: -4} != ".pdf" ] || [ $basename != cluster*]; then 
     tar -xvf "$basename" --one-top-level=="$basename" 
-  fi
-  if [[ ! $basename =~ "=" ]] ; then # if not a directory, move to /cluster
-    mv $basename cluster17
+  else # if not a directory, move to /cluster
+    mv $basename ./cluster/
   fi
 done
 
--- src/usefilter.sh
+++ src/usefilter.sh
@@ -1,12 +1,14 @@
 # pandoc ../data/nat_orders_revisionv4Amaro.tex -f latex -t html -s -o non-filtered.html
-# cwd: results/1701/
-set -e # Stop script if a simple command fails
-for basename in =1701.*_debib.tex; do
-    pandoc $basename -f latex -t opml \
-    --filter /home/local/yzan/Desktop/Cleanskin/src/pandocFilter.py \
-    --template=/home/local/yzan/Desktop/Cleanskin/data/temp.xml \
-    -s -o 1701xml/${basename:0:11}_filtered.xml
-done
+# cwd: /src/
+if cd ../results/0001; then
+    # set -e # Stop script if a simple command fails
+    for basename in =*0001*_debib.tex; do
+        pandoc $basename -f latex -t opml \
+        --filter /home/local/yzan/Desktop/Cleanskin/src/pandocFilter.py \
+        --template=/home/local/yzan/Desktop/Cleanskin/data/temp.xml \
+        -s -o 0001filtered/${basename:0:-10}_filtered.xml
+    done
+fi
 
 # pandoc -s ../data/nat_orders_revisionv4Amaro.tex -o paper.html
 # pandoc -s -t JSON ../data/nat_orders_revisionv4Amaro.tex > pandoAST.txt # Check the AST of the file
