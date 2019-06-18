7166b86251d081e5905ac0d6dbc247f4fd659924
karoteeni110 <akb48auy@sohu.com>
2019-06-18 16:58:48 +0300

    Output names

--- src/xmlCleaner.py
+++ src/xmlCleaner.py
@@ -1,29 +1,31 @@
 """
-Ruling out the <acknowledgement> and <references> sections.
-Flattening the subsections.
+Rules out the <acknowledgement> and <references> sections.
+Flattens the subsections.
 
-Handles at most one layer subsection structure.
+Does NOT assume single layer subsection structure.
 """
 import xml.etree.ElementTree as ET
-from collections import defaultdict
 from unicodedata import normalize
+from os.path import basename, join
+from paths import within_results
+import sys
 
-fpath = '/home/yzan/Desktop/Cleanskin/results/0002/0002xml/astro-ph0002005.xml'
+fpath = sys.argv[1] # abspath of the xml
+if fpath[-4:] != '.xml':
+    raise AssertionError ("Not XML!")
 data = open(fpath,'r')
 tree = ET.parse(data)
 root = tree.getroot()
 
 def recur_rm(parent):
-    '''
-    Check and remove the acknowledgement sec in the children.
+    '''Check and remove the acknowledgement sec in the children.
     '''
     for child in list(parent): # If no child, list is empty
         if child.get('text', False).lower() in ('acknowledgements', 'references', 'bibliography'):
             parent.remove(child)
 
 def flatten(parent):
-    '''
-    Organize subsections into one whole section.
+    '''Organize subsections into one whole section.
     '''
     if parent.tag == 'outline':
         for subsec in list(parent): # If no child, list is empty
@@ -31,30 +33,11 @@ def flatten(parent):
             parent.remove(subsec)
 
 # title, abstract, sections['introduction'] ... 
-sections = defaultdict(str)
 for child in root: # head, body
-    if child.tag == 'head':
-        title = child[0].text
-        abstract = child[1].text
-
-    elif child.tag == 'body':
+    if child.tag == 'body':
         for outline in child.iter():
             recur_rm(outline)
             flatten(outline)
 
-            # if secOrSubsec.attrib['text'].lower() in ('acknowledgements', 'references'):
-            #     secOrSubsec.clear()
-            #     continue
-        # for outline in child:
-        #     if len(outline.attrib.keys()) == 2: # 'text', '_note'
-        #         sections[outline.attrib['text'].lower()] = outline.attrib['_note']
-        #     elif len(outline.attrib.keys()) == 1:
-        #         for subsec in outline:
-        #             sections[outline.attrib['text'].lower()] += '\n' + subsec.attrib['_note']
-
-    # print('Title', title)
-    # print('==')
-    # print('abstract', abstract)
-    # print('==')
-tree.write('/home/yzan/Desktop/0002005.xml')
-# print(normalize(u'\xa0', sections['introduction']))
+outname = join(within_results('/final'), basename(fpath))
+tree.write(outname) # Overwrite the original file
