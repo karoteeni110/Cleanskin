0ea225bd429f23682ccfe8e04e6fd8542f763764
karoteeni110 <akb48auy@sohu.com>
2019-06-19 14:57:52 +0300

    usage, dddd variable

--- src/usefilter.sh
+++ src/usefilter.sh
@@ -1,13 +1,15 @@
-# pandoc ../data/nat_orders_revisionv4Amaro.tex -f latex -t html -s -o non-filtered.html
-# pwd: /src
+# Usage:
+# usefilter.sh 2> ../results/dddd/log/xmlerror.txt 
 
-if cd ../results/0002; then
+# pwd: /src
+dddd=0002
+if cd ../results/${dddd}; then
     # set -e # Stop script if a simple command fails
-    for basename in =*0002*_debib.tex; do
+    for basename in =*${dddd}*_debib.tex; do
         pandoc $basename -f latex -t opml \
         --filter /home/local/yzan/Desktop/Cleanskin/src/pandocFilter.py \
         --template=/home/local/yzan/Desktop/Cleanskin/data/temp.xml \
-        -s -o 0002filtered/${basename:0:-10}_filtered.xml
+        -s -o ${dddd}filtered/${basename:0:-10}_filtered.xml
         if [ "$?" = "0" ]; then
             echo 'FILE:' $basename 'MESSAGE:' 1&>2
     done
@@ -15,3 +17,4 @@ fi
 
 # pandoc -s ../data/nat_orders_revisionv4Amaro.tex -o paper.html
 # pandoc -s -t JSON ../data/nat_orders_revisionv4Amaro.tex > pandoAST.txt # Check the AST of the file
+# pandoc ../data/nat_orders_revisionv4Amaro.tex -f latex -t html -s -o non-filtered.html
