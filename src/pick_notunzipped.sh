baab7d92cbe6b32cfaf34409e57cd1eaa974f262
karoteeni110 <akb48auy@sohu.com>
2019-06-20 11:38:52 +0300

    0001:2364; 0002:2365; 1701:414 in /try

---
+++ src/pick_notunzipped.sh
@@ -0,0 +1,25 @@
+# !/bin/bash
+
+dddd='1701'
+
+# ========================
+
+if cd /home/yzan/Desktop/arXiv/$dddd ; then
+  pwd
+else
+  echo 'Could not change directory! Aborting.' 1>&2
+  exit 1
+fi
+# CHANGE FnArray for every dddd
+declare -a FnArray=('1701.01247' '1701.01292' '1701.01020')
+
+for fn in "${FnArray[@]}"; do
+    cp -i $fn.gz ../../try/${dddd}short/ # be warned before overwriting
+done
+
+# ========================
+
+if cd /home/local/yzan/Desktop/try/${dddd}short; then
+    for i in *.gz; do mkdir ${i:0:-3}; done
+    for i in *.gz; do mv $i ${i:0:-3}; cd $_ ; gunzip -rdN $i; cd ..; done
+fi
