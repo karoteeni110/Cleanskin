41d33dca1e89b2c2265b995fa70ee386e6f08aaf
karoteeni110 <akb48auy@sohu.com>
2019-06-25 10:53:23 +0300

    1701 results record

--- src/conv.sh
+++ src/conv.sh
@@ -26,40 +26,37 @@ noRefs () {
 
 # =========================
 
-dddd='1701'
-
 function call_debib {
     for tex in data/$dddd/*/*.tex; do
-        OUT=results/$dddd/supbib/$(basename "$(dirname "$tex")")/$(basename "$tex")
+        OUT=results/$dddd/1/$(basename "$(dirname "$tex")")/$(basename "$tex")
         suppress_bib $tex $OUT
     done
 }
 
-# call_debib
+
 
 function call_converter {
-    for tex in results/$dddd/supbib/*/*.tex; do
+    for tex in results/$dddd/1/*/*.tex; do
         DIRNAME2=$(basename "$(dirname "$tex")")/$(basename "$tex")
-        OUT=results/$dddd/xml1/${DIRNAME2:0:-4}.xml
+        OUT=results/$dddd/2/${DIRNAME2:0:-4}.xml
     if mkdir $(dirname "$OUT"); then
         convert $tex $OUT
     else
         convert $tex $OUT
     fi
     if [ "$?" -eq "0" ]; then
-        echo 'FILE:' $1 'MESSAGE:' 1&>2
+        echo 'FILE:' $1 'MESSAGE:' 1&>2 # TODO: show the failed
     fi
     done
 }
 
-# call_converter 2>>results/$dddd/xml1/log.txt
+
 
 function postclean {
-    for xml in results/$dddd/xml1/*/*.xml; do
+    for xml in results/$dddd/2/*/*.xml; do
         DIRNAME2=$(basename "$(dirname "$xml")")/$(basename "$xml")
         OUT=results/$dddd/3/${DIRNAME2:0:-4}.xml
-
-        #python3 src/regexCleaner.py postclean $xml $OUT 
+        python3 src/regexCleaner.py postclean $xml $OUT 
         python3 src/xmlCleaner.py $OUT results/$dddd/fi/${DIRNAME2:0:-4}.xml
 
         if [ "$?" -eq "0" ]; then
@@ -68,4 +65,7 @@ function postclean {
     done
 }
 
+dddd='1701'
+# call_debib |
+# call_converter 2>>results/$dddd/2/log.txt |
 postclean #2>>results/$dddd/fi/log.txt
