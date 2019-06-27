d59d8928edf566b0f75245e8ea10cffff1e6a1d4
karoteeni110 <akb48auy@sohu.com>
2019-06-27 16:45:40 +0300

    usefilter DONE

--- src/regexCleaner.py
+++ src/regexCleaner.py
@@ -3,15 +3,12 @@ Cleans {thebibliography} section before using Pandoc
 OR 
 the XML characters (e.g. '&#10;', '&amp;') and refs ('\[sec:summery\]') 
 after it.
-
 Output:
 debib: writes out cleaned tex files into /result and rename them in the format
 e.g. '=1701.01278_debib.tex'.
 postclean: writes out into /result/DIRNAME and rename them in the format
 e.g. '=1701.01278.xml'
-
 Assumes all the latex file end with ".tex".
-
 Usage:
     python3 regexCleaner.py $INPUT_FILENAME $OUTPUT_FILENAME
     python3 regexCleaner.py $INPUT_FILENAME $OUTPUT_FILENAME
@@ -22,10 +19,6 @@ from os import remove, mkdir, walk
 from paths import data_path
 from shutil import copyfile
 import fnmatch
-
-def get_artIDdirs_in_data(dddd):
-    artIDs = next(walk(join(data_path, dddd)))
-    return artIDs[1]
 
 def subout(inpath, outpath, pt_pairs):
     with open(inpath, mode='r', encoding='utf-8', errors='ignore') as texfile:
@@ -40,74 +33,3 @@ def subout(inpath, outpath, pt_pairs):
             pass
         with open(outpath,'w') as out:
             out.write(clean_data)
-
-def iter_dirs(anotherFunc, extraArgs, rootdir, fnpattern, out_suffix, capture_err_type, errlog_path):
-    errlog = open(errlog_path, 'a')
-    for rt, _, fls in walk(rootdir):
-        for itm in fnmatch.filter(fls, fnpattern):
-            inpath = join(rt, itm)
-            outpath = 'results' +inpath.strip('data').strip('.tex') + out_suffix
-            try:
-                anotherFunc(inpath, outpath, *extraArgs)
-            except capture_err_type:
-                copyfile(inpath, errlog_path)
-                errlog.write()
-
-
-
-def main(inputdir, subPtDict, errlogpath):
-    """
-    subPtDict: dictionary, in the form of {pattern: subbed_string}. 
-    inputdir: 'data/1701'
-    logdir: directory path for errlog and problematic files.
-    """
-    # Read data & handle exceptions
-    ovw_err_log = open(errlogpath, 'a')
-    for rt, _, fls in walk(inputdir):
-        for itm in fnmatch.filter(fls, '*.tex'):
-            inputpt = join(rt, itm)
-            outputpt = 'results' + inputpt.strip('data').strip('.tex') + '_db.tex'
-            try:
-                subout(inputpt, subPtDict)
-            except:
-                copyfile(inputpt, dirname(errlogpath))
-                for i in sys.exc_info():
-                    ovw_err_log.write(str(i)+ ' ')
-                ovw_err_log.write(' \n')
-    ovw_err_log.write('================================ \n')
-    ovw_err_log.close()
-
-if __name__ == "__main__":
-    # VERBOSE, REPORT_EVERY = True, 100
-    dddd = 'data/1701'
-    errlogpath = 'results/%s/log/debibErr.txt' % dddd[-4:]
-    bibPt = {r'(\\begin(\*)?{thebibliography}.*?\\end(\*)?{thebibliography})':''}
-    try:
-        mkdir(dirname(errlogpath))
-    except FileExistsError:
-        pass
-    main(dddd, bibPt, errlogpath)
-
-
-    """
-    >>> for rt, dirn, fls in walk('1701short'):
-    ...     print(rt)
-    ...     print(dirn, fls)
-    ...     print()
-    ... 
-    1701short
-    ['1701.01247', '1701.01020', '1701.01292'] []
-
-    1701short/1701.01247
-    [] ['withdrawn']
-
-    1701short/1701.01020
-    [] ['withdrawn']
-
-    1701short/1701.01292
-    ['test'] ['withdrawn']
-
-    1701short/1701.01292/test
-    [] ['try']
-
-    """
--- src/useFilter.py
+++ src/useFilter.py
@@ -17,17 +17,52 @@
 # done
 
 from subprocess import run, PIPE
+from shutil import copyfile, copytree
+from os import walk, mkdir, stat
+from os.path import basename, dirname
 
-fromfile, tofile = '', ''
-a = run('pandoc %s -f latex -t opml\
-    --filter src/pandocFilter.py\
-    --template=data/temp.xml\
-    -s -o %s' % (fromfile, tofile), \
-    shell=True, stdout=PIPE, stderr=PIPE)
-for i in [a.stderr, a.stdout]:
-    if i != b'': # stderr an stdout should BOTH be null if convertion succeeds
+def pandoc(from_fpath, to_fpath):
+    a = run('pandoc %s -f latex -t opml\
+        --filter src/pandocFilter.py\
+        --template=data/temp.xml\
+        -s -o %s' % (from_fpath, to_fpath), shell=True, stdout=PIPE, stderr=PIPE)
+    return a
 
+def errLog(cmd, inputfile, outfile, errFileDest, errlog):
+    for stdmsg in [cmd.stderr, cmd.stdout]:
+        if stdmsg != b'': # stderr an stdout should BOTH be null if convertion succeeds
+            try:
+                copyfile(inputfile, errFileDest + '/%s' % basename(inputfile))
+            except FileNotFoundError:
+                copytree(dirname(inputfile), errFileDest + '/%s' % basename(dirname(inputfile)))
+            errlog.write(inputfile + ' \n' + stdmsg.decode('utf-8'))
+            errlog.write('========================================= \n')
+        elif stat(outfile):
+            errlog.write(inputfile + ' Convertion fails: Output empty.')
+            errlog.write('========================================= \n')
 
+def mk_the_dirs(dirpaths):
+    for i in dirpaths:
+        try:
+            mkdir(i)
+        except FileExistsError:
+            pass
+    
+def main():
+    rootdir = 'results/1701/db'
+    articles = next(walk(rootdir))[1]
+    errFileDir = rootdir + '/errCases'
+    elp = errFileDir + '/log.txt'
+    mk_the_dirs([errFileDir])
+
+    with open(elp, 'a') as errlog:
+        for art in articles:
+            fromfile = rootdir + art + '/*.tex'
+            tofile = rootdir + '/opml/' + art + '.xml'
+            cmd = pandoc(fromfile, tofile)
+            errLog(cmd, fromfile, tofile, errFileDir, errlog)
+
+main()
 # pandoc -s ../data/nat_orders_revisionv4Amaro.tex -o paper.html
 # pandoc -s -t JSON ../data/nat_orders_revisionv4Amaro.tex > pandoAST.txt # Check the AST of the file
 # pandoc ../data/nat_orders_revisionv4Amaro.tex -f latex -t html -s -o non-filtered.html
