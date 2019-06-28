994d9fc14ea80db9181fc71cce4fcbd3b5627f77
karoteeni110 <akb48auy@sohu.com>
2019-06-28 12:09:26 +0300

    useFilter.py DONE

--- src/useFilter.py
+++ src/useFilter.py
@@ -18,8 +18,8 @@
 
 from subprocess import run, PIPE
 from shutil import copyfile, copytree
-from os import walk, mkdir, stat
-from os.path import basename, dirname
+from os import walk, mkdir, stat, remove
+from os.path import basename, dirname, exists
 
 def pandoc(from_fpath, to_fpath):
     a = run('pandoc %s -f latex -t opml\
@@ -28,18 +28,21 @@ def pandoc(from_fpath, to_fpath):
         -s -o %s' % (from_fpath, to_fpath), shell=True, stdout=PIPE, stderr=PIPE)
     return a
 
-def errLog(cmd, inputfile, outfile, errFileDest, errlog):
+def check_err(cmd, inputfile, outfile, errFileDest, errlog):
     for stdmsg in [cmd.stderr, cmd.stdout]:
         if stdmsg != b'': # stderr an stdout should BOTH be null if convertion succeeds
-            try:
-                copyfile(inputfile, errFileDest + '/%s' % basename(inputfile))
-            except FileNotFoundError:
-                copytree(dirname(inputfile), errFileDest + '/%s' % basename(dirname(inputfile)))
+            # try:
+            #     copyfile(inputfile, errFileDest + '/%s' % basename(inputfile))
+            # except FileNotFoundError:
+            copytree(dirname(inputfile), errFileDest + '/%s' % basename(dirname(inputfile)))
             errlog.write(inputfile + ' \n' + stdmsg.decode('utf-8'))
             errlog.write('========================================= \n')
-        elif stat(outfile):
-            errlog.write(inputfile + ' Convertion fails: Output empty.')
-            errlog.write('========================================= \n')
+
+            if exists(outfile):
+                if stat(outfile).st_size == 0:
+                    remove(outfile)
+                # errlog.write(inputfile + ' Invisible failure : Output empty.')
+                # errlog.write('========================================= \n')
 
 def mk_the_dirs(dirpaths):
     for i in dirpaths:
@@ -51,17 +54,17 @@ def mk_the_dirs(dirpaths):
 def main():
     rootdir = 'results/1701/db'
     articles = next(walk(rootdir))[1]
-    errFileDir = rootdir + '/errCases_fi'
+    errFileDir = rootdir.strip('db') + 'errCases_opml'
     elp = errFileDir + '/log.txt'
     
     mk_the_dirs([errFileDir])
 
     with open(elp, 'a') as errlog:
         for art in articles:
-            fromfile = rootdir + art + '/*.tex'
-            tofile = rootdir + '/opml/' + art + '.xml'
+            fromfile = rootdir + '/' + art + '/*.tex'
+            tofile = rootdir.strip('db') + 'opml/' + art + '.xml'
             cmd = pandoc(fromfile, tofile)
-            errLog(cmd, fromfile, tofile, errFileDir, errlog)
+            check_err(cmd, fromfile, tofile, errFileDir, errlog)
 if __name__ == "__main__":
     main()
 # pandoc -s ../data/nat_orders_revisionv4Amaro.tex -o paper.html
