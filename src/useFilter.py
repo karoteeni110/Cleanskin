a75cd95fe5f146809cc20b2d425f4434b6b2a313
karoteeni110 <akb48auy@sohu.com>
2019-06-28 15:58:00 +0300

    errcase_redo() fixed

--- src/useFilter.py
+++ src/useFilter.py
@@ -17,7 +17,7 @@
 # done
 
 from subprocess import run, PIPE
-from shutil import copyfile, copytree
+from shutil import copyfile, copytree, rmtree
 from os import walk, mkdir, stat, remove
 from os.path import basename, dirname, exists
 
@@ -25,24 +25,33 @@ def pandoc(from_fpath, to_fpath):
     a = run('pandoc %s -f latex -t opml\
         --filter src/pandocFilter.py\
         --template=data/temp.xml\
-        -s -o %s' % (from_fpath, to_fpath), shell=True, stdout=PIPE, stderr=PIPE)
+        -s -o %s 2>&1' % (from_fpath, to_fpath), shell=True, stdout=PIPE)
     return a
 
 def check_err(cmd, inputfile, outfile, errFileDest, errlog):
-    for stdmsg in [cmd.stderr, cmd.stdout]:
-        if stdmsg != b'': # stderr an stdout should BOTH be null if convertion succeeds
-            # try:
-            #     copyfile(inputfile, errFileDest + '/%s' % basename(inputfile))
-            # except FileNotFoundError:
-            copytree(dirname(inputfile), errFileDest + '/%s' % basename(dirname(inputfile)))
-            errlog.write(inputfile + ' \n' + stdmsg.decode('utf-8'))
-            errlog.write('========================================= \n')
+    if cmd.stdout != b'': # stderr an stdout should BOTH be null if convertion succeeds
+        # try:
+        #     copyfile(inputfile, errFileDest + '/%s' % basename(inputfile))
+        # except FileNotFoundError:
+        copytree(dirname(inputfile), errFileDest + '/%s' % basename(dirname(inputfile)))
+        errlog.write(inputfile + ' \n' + cmd.stdout.decode('utf-8'))
+        errlog.write('========================================= \n')
 
-            if exists(outfile):
-                if stat(outfile).st_size == 0:
-                    remove(outfile)
-                # errlog.write(inputfile + ' Invisible failure : Output empty.')
-                # errlog.write('========================================= \n')
+        if exists(outfile):
+            if stat(outfile).st_size == 0:
+                remove(outfile)
+
+def check_redo_err(cmd, inputfile, outfile, errFileDest, errlog):
+    if cmd.stdout != b'': # stderr an stdout should BOTH be null if convertion succeeds
+        errlog.write(inputfile + ' \n' + cmd.stdout.decode('utf-8'))
+        errlog.write('========================================= \n')
+    
+    if exists(outfile):
+        if stat(outfile).st_size == 0:
+            remove(outfile)
+        else:
+            rmtree(dirname(inputfile))
+            print('%s; Done & rm: %s ' % (cmd.stdout, dirname(inputfile)))
 
 def mk_the_dirs(dirpaths):
     for i in dirpaths:
@@ -54,7 +63,7 @@ def mk_the_dirs(dirpaths):
 def main():
     rootdir = 'results/1701/db'
     articles = next(walk(rootdir))[1]
-    errFileDir = rootdir.strip('db') + 'errCases_opml'
+    errFileDir = dirname(rootdir) + 'errCases_opml'
     elp = errFileDir + '/log.txt'
     
     mk_the_dirs([errFileDir])
@@ -62,12 +71,28 @@ def main():
     with open(elp, 'a') as errlog:
         for art in articles:
             fromfile = rootdir + '/' + art + '/*.tex'
-            tofile = rootdir.strip('db') + 'opml/' + art + '.xml'
+            tofile = dirname(rootdir) + 'opml/' + art + '.xml'
             cmd = pandoc(fromfile, tofile)
             check_err(cmd, fromfile, tofile, errFileDir, errlog)
-            
+
+def errcase_redo():
+    rootdir = 'results/1701/errCases_opml'
+    articles = next(walk(rootdir))[1]
+    errFileDir = rootdir
+    elp = errFileDir + '/REDOlog.txt'
+    mk_the_dirs([errFileDir])
+
+    with open(elp, 'a') as errlog:
+        for art in articles:
+            if art[-1].isdigit():
+                fromfile = rootdir + '/' + art + '/*.tex'
+                tofile = dirname(rootdir) + '/opml/' + art + '.xml'
+                cmd = pandoc(fromfile, tofile)
+                check_redo_err(cmd, fromfile, tofile, errFileDir, errlog)
+
+
 if __name__ == "__main__":
-    main()
+    errcase_redo()
 # pandoc -s ../data/nat_orders_revisionv4Amaro.tex -o paper.html
 # pandoc -s -t JSON ../data/nat_orders_revisionv4Amaro.tex > pandoAST.json # Check the AST of the file
 # pandoc ../data/nat_orders_revisionv4Amaro.tex -f latex -t html -s -o non-filtered.html
