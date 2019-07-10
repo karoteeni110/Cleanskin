e45d5c7c7687c3daa1e1feef442df1937f4e0960
karoteeni110 <akb48auy@sohu.com>
2019-07-10 18:01:28 +0300

    get_all_rank1 in check_patters

--- src/checkPatterns.py
+++ src/checkPatterns.py
@@ -2,33 +2,47 @@ from paths import results_path
 from os.path import join
 from os import listdir
 import xml.etree.ElementTree as ET
+from collections import Counter
+import pickle
 
-def get_rank1(rootdir, report=False, node=None):
+def get_rank1(fpath): 
+    rank1nodes = []           
+    try:
+        tree = ET.parse(fpath)
+        root = tree.getroot()
+        elemlst = [elem.tag[elem.tag.find('}')+1:] for elem in list(root)] # get rid of namespace
+        rank1nodes.extend(elemlst)
+    except ET.ParseError:
+        print('ParseError at',fpath)
+    return frozenset(rank1nodes)
+
+def get_all_rank1(rootdir, report_every=1, dumpnew = True, pkl = None):
     '''
     Collect all the possible direct children of <document>.
     '''
-    rank1 = []
-    for fn in listdir(rootdir):
-        fpath = join(rootdir, fn)
-        if fn[-3:] == 'xml': 
-            try:
-                tree = ET.parse(fpath)
-                root = tree.getroot()
-                elemlst = [elem.tag[elem.tag.find('}')+1:] for elem in list(root)]
-                rank1.extend(elemlst)
-            except ET.ParseError:
-                print('ParseError at',fpath)
-                continue
-        if report:
-            if node in set(elemlst):
-                print('Found node %s in %s' % (node, fpath))
-    return set(rank1)
+    if not pkl:
+        nodesetlst = []
+        print('Collecting doc infos....')
+        for i, fn in enumerate(listdir(rootdir)):
+            fpath = join(rootdir, fn)
+            if fn[-3:] == 'xml': 
+                nodesetlst.append(get_rank1(fpath))
+            if (i+1) % report_every == 0:
+                print('File %s of %s collected.' % (i+1, len(listdir(rootdir))))
+        rank1nodes = Counter(nodesetlst)
+    else:
+        reak1nodes = pickle.load(pkl)
+            
+    # Print out 
+    for a in rank1nodes:
+        print(rank1nodes[a], list(a))
+            
 
 
 if __name__ == "__main__":
-    travdir = join(results_path, 'latexml/1701')
+    travdir = join(results_path, 'latexml')
     # print(get_rank1(travdir))
-    get_rank1(travdir)
+    get_all_rank1(travdir, 50)
     # 1701:
     # ParseError at =1701.01158.xml
     # ParseError at =1701.00987.xml
--- src/useLatexml.py
+++ src/useLatexml.py
@@ -17,7 +17,18 @@ def pick_toptex(artdir):
     out = a.stdout.decode('utf-8').split('\n')[0]
     return out
 
-def trav_data(rootdir, errlog, excluded_dirs=None):
+def trav_data(rootdir, errlog, outdir, excluded_dirs=None):
+    '''
+    Traverse all article directories in ``rootdir``, 
+    getting the largest .tex / .ltx file converted to XMLs with latexml.
+    XMLs will be written out in ``outdir``
+
+    If the article:
+    a) is in TeX format: 
+           ==> processed by latexml (output => /outdir/ )
+        or ==> in ``excluded_dirs`` (latexml hangs when processing it): cp to /excluded
+    b) not in TeX format:
+    '''
     dirs = listdir(rootdir)
     
     for ith, art in enumerate(dirs):
@@ -26,39 +37,43 @@ def trav_data(rootdir, errlog, excluded_dirs=None):
         if art[-1].isdigit() and art not in excluded_dirs: # the folder name of articles should consist of digits
             toptex_fn = pick_toptex(art_path)
             toptex_path = join(art_path, toptex_fn)
-            output_path = join(results_path, 'latexml/%s.xml' % art) # XXX: CHANGE IT
+            output_path = join(outdir, '%s.xml' % art) # XXX: CHANGE IT
             
             if toptex_fn != '': 
                 if not exists(output_path): # Avoid overwriting
                     print(art + ' --> %s of %s' % (ith+1, len(dirs)))
                     cmd = latexml(toptex_path, output_path)
                     if cmd.stdout != b'':
-                        print(art_path)
-                        print(cmd.stdout)
-                        errlog.write(art_path + '\n' + cmd.stdout)
+                        print('latexml err:', art_path)
+                        # print(cmd.stdout.decode('utf-8'))
+                        errlog.write(art_path + '\n' + cmd.stdout.decode('utf-8'))
                         errlog.write('================================== \n')
                 else:
                     print('File exists. Skip %s' % toptex_path)
-            else:
-                err_art_dest = join(results_path, 'errcp/notex/%s' % art)
-                makedirs(dirname(err_art_dest), exist_ok=True) # check if parent dir exists
+                    #pass
+            else: # Articles that are not in LaTex
+                err_art_dest = join(errcase_path, 'notex/%s' % art)
                 print('Tex not found. Cp %s to %s' % (art, err_art_dest) )
-                copytree(art_path, err_art_dest)
-        else:
-            dest0 = join(err_art_dest, 'excluded/%s' % art)
+                makedirs(dirname(err_art_dest), exist_ok=True) # check if parent dir exists
+                if not exists(err_art_dest):
+                    copytree(art_path, err_art_dest)
+        else: # TeX cannot be processed by latexml
+            dest0 = join(errcase_path, 'excluded/%s' % art)
             makedirs(dirname(dest0), exist_ok=True) # check if parent dir exists
-            copytree(art_path, dest0)
+            if not exists(dest0):
+                copytree(art_path, dest0)
             print('Excluded dir %s cp to %s' % (art, dest0))
 
 if __name__ == "__main__":
-    errlogpath = join(results_path, 'latexml/logs/errcase_redo.txt')
-    rootdir = errcase_path # XXX: CHANGE IT
+    errlogpath = join(results_path, 'latexml/logs/log.txt')
+    rootdir = data1701 # XXX: CHANGE IT
+    outdir = join(results_path, 'latexml')
     excluded_arts = ['=hep-ex0001041', '=astro-ph0001216', '=astro-ph0001480'] \
-                    + ['=astro-ph0002515'] # XXX:CHANGE IT 
+                    + ['=astro-ph0002515']
     
     start = time.time()
     with open(errlogpath, 'a') as errlog:
-        trav_data(rootdir, errlog, excluded_arts)          
+        trav_data(rootdir, errlog, outdir, excluded_arts)          
     end = (time.time() - start)/3600
     print('Used time: %s hs' % end)
 
