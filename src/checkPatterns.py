754951622899a4427f68c7280444ac5f070cfe06
karoteeni110 <akb48auy@sohu.com>
2019-07-11 17:15:22 +0300

    root.findall() + XPath

--- src/checkPatterns.py
+++ src/checkPatterns.py
@@ -5,14 +5,14 @@ from os import listdir
 import xml.etree.ElementTree as ET
 from collections import Counter
 import pickle
-
-
+    
 def get_rank1(fpath): 
     rank1nodes = []           
     try:
         tree = ET.parse(fpath)
         root = tree.getroot()
-        elemlst = [elem.tag[elem.tag.find('}')+1:] for elem in list(root)] # get rid of namespace
+        ignore_ns(root)
+        elemlst = [elem.tag for elem in root.findall('./*')] # get rid of namespace
         rank1nodes.extend(elemlst)
     except ET.ParseError:
         print('ParseError at',fpath)
@@ -39,33 +39,44 @@ def get_all_rank1(rootdir=None, report_every=100, newpkl=None, oldpkl=None):
         rank1nodes = pickle.load(open(oldpkl, 'rb'))
     return rank1nodes
     
-def show_most_common(nodes, first):
-    for st, ct in nodes.most_common(first):
+def show_most_common(counterdict, first):
+    for st, ct in counterdict.most_common(first):
         print(ct, list(st))
 
-def show_examplefile(rootdir, tagname):
+def show_examplefile(rootdir, tagname, rank1only=True):
     '''
     Show example files that contain the tags in ``tagname``.
     '''
     for fn in listdir(rootdir):
         fpath = join(rootdir, fn)
         if fn[-3:] == 'xml':
-            nodes = get_rank1(fpath)
-        if set(tagname) <= nodes:
+            try:
+                tree = ET.parse(fpath)
+                root = tree.getroot()
+                ignore_ns(root)
+                if rank1only:
+                    elem = root.findall(tagname)
+                else:
+                    elem = root.findall('.//%s'% tagname)
+            except ET.ParseError:
+                print('ParseError at',fpath)
+                elem = []
+        
+        if elem != []: 
             print('Found %s in %s' % (tagname, fpath))
+            
 
 def check_children(parent_tag, fpath):
-    allnodes = []           
+    direct_children_tags = []           
     try:
         tree = ET.parse(fpath)
         root = tree.getroot()
-        ignore_ns(root)
-        child_taglst = [child.tag for parent in root.iter(parent_tag) 
-                                    for child in list(parent)] # get rid of namespace
-        allnodes.extend(child_taglst)
+        ignore_ns(root) # get rid of namespace
+        child_taglst = [child.tag for child in root.findall('.//%s/*' % parent_tag)] 
+        direct_children_tags.extend(child_taglst)
     except ET.ParseError:
         print('ParseError at',fpath)
-    return frozenset(allnodes)
+    return frozenset(direct_children_tags)
 
 def check_children_iter(rootdir, parent_tag, report_every=100, newpkl=None, oldpkl=None):
     if not oldpkl:
@@ -93,14 +104,14 @@ if __name__ == "__main__":
     # show_most_common(nodes,20)
     # show_examplefile(rootdir, ['resource', 'abstract', 'section', 'title', 'creator', 'bibliography'])
     # show_examplefile(rootdir, ['para', 'title', 'creator', 'resource', 'ERROR', 'section', 'abstract'])
-    
-    fd_pkl = join(data_path, 'paraChildren.pkl')
-    freqdist = check_children_iter(rootdir, 'para', oldpkl=fd_pkl)
-    show_most_common(freqdist, 20)
+
+    # fd_pkl = join(data_path, 'paraChildren.pkl')
+    # freqdist = check_children_iter(rootdir, 'para', oldpkl=fd_pkl)
+    # show_most_common(freqdist, 20)
 
 
 
-    
+
     # 1701:
     # ParseError at =1701.01158.xml
     # ParseError at =1701.00987.xml
