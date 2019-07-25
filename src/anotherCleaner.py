81d26bb71429d0895c6fd516e196f6a569dd48b5
karoteeni110 <akb48auy@sohu.com>
2019-07-25 18:41:39 +0300

    tofix:  /results/=math0001145.xml

--- src/anotherCleaner.py
+++ src/anotherCleaner.py
@@ -10,7 +10,7 @@ from xmlCleaner import ignore_ns, get_root, get_ttn
 
 keeplist = ['title', 'abstract', 'section', 'creator', 'keywords', 'titlepage', 'para', 'chapter', 'bibliography', \
                     'paragraph', 'subparagraph', 'subsection', 'appendix', 'theorem', 'proof', 'subsubsection']
-may_contain_subsecs = ['section', 'chapter', 'subsection', 'subsubsection', 'paragraph', 'subpragraph']
+sec_tags = ['section', 'chapter', 'subsection', 'subsubsection', 'paragraph', 'subpragraph', 'appendix', 'bibliography']
 
 
 def remove_useless(root, tags = ['cite', 'Math', 'figure', 'table', 'TOC', 'ERROR', 'pagination', 'rdf', 'index', \
@@ -26,25 +26,62 @@ def remove_useless(root, tags = ['cite', 'Math', 'figure', 'table', 'TOC', 'ERRO
             elem.tag = 'p'
             elem.text = txt
 
+def flatter_elem(elem):
+    txt = ''.join(elem.itertext())
+    elem.clear()
+    elem.text = txt
+
+def clean_sec(sec):
+    for subelem in sec:
+        flatter_elem(subelem)
+
+def have_title(elem):
+    if elem.findall('title') != []:
+        return True
+    else:
+        return False
+
+def have_subsec(elem):
+    for subelem in elem:
+        if subelem.tag in sec_tags and have_title(subelem):
+            return True
+    return False
+
 def texify(root):
     toremove = []
-    for rank1elem in root:
-        if rank1elem.tag in keeplist:
-            # TODO: if there is no subsection -type elements
-            txt = ''.join(rank1elem.itertext())
-            title, subtitle = rank1elem.find('title'), rank1elem.find('subtitle')
-            rank1elem.clear()
-            rank1elem.text = txt
-            for t in [title, subtitle]:
-                if t != None:
-                    rank1elem.set(t.tag, ''.join(t.itertext()))
-            if rank1elem.tag in may_contain_subsecs and rank1elem.get('title', None):
-                rank1elem.tag = 'section'
-        else:
-            toremove.append(rank1elem) # Don't modify 
-    for i in toremove:
-        root.remove(i)
 
+    for rank1elem in root:  # 1st pass
+        if rank1elem.tag in keeplist: # if element in ``keeplist``, texify it; remove otherwise
+
+            
+            if have_subsec(rank1elem):
+                for subelem in rank1elem:
+                    if subelem.tag in sec_tags:
+                        clean_sec(subelem)
+                    else:
+                        flatter_elem(rank1elem)
+
+            elif rank1elem.tag in sec_tags: # sec that don't have subsecs 
+                clean_sec(rank1elem)
+                rank1elem.tag = 'section'
+            else:
+                flatter_elem(rank1elem)
+        else:
+            toremove.append((root, rank1elem)) # Don't modify it during iteration!
+
+    for title_parent in root.findall('.//title/..'): # 2nd pass
+        title, subtitle = title_parent.find('title'), title_parent.find('subtitle')
+        for t in [title, subtitle]:
+            if t != None:
+                title_parent.set(t.tag, ''.join(t.itertext()))
+                toremove.append((title_parent, title))
+
+    
+    for p, c in toremove:
+        try:
+            p.remove(c)
+        except ValueError:
+            print(p, c)
 if __name__ == "__main__":
     # xmls = [fn for fn in listdir(rawxmls_path) if fn[-4:] == '.xml']
     xmls = ['=math0001145.xml']
