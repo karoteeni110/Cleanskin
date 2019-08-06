d9e32a6404e7088ac6afdaa0a30de09695110a9c
karoteeni110 <akb48auy@sohu.com>
2019-08-06 16:52:37 +0300

    section count per article

--- src/headingStats.py
+++ src/headingStats.py
@@ -63,14 +63,16 @@ def show_cates_per_art():
     for count in cter:
         print('%s articles associated with %s categories: %.4f ' % (cter[count], count, (cter[count]/sum(cter.values()))))
 
-def show_secnum_per_art():
+def show_seccount_per_art():
     sec_nums = []
     for xmlpath in xmlpath_list:
         sec_num = len(get_headings(xmlpath)['section'])
         sec_nums.append(str(sec_num))
     cter = Counter(sec_nums)
-    print(cter['1'])
-    print('Not 0:', sum(cter.values())-cter['0'])
+    print(cter)
+    # non_zerosec_avg = sum(int(k)*cter[k] for k in cter if k != '0') / (sum(cter.values())-cter['0'])
+    avg = sum(int(k)*cter[k] for k in cter) / sum(cter.values())
+    print('Average: %s sections per article' % avg)
 
 def show_sectitle_per_art():
     sectitle_cter = Counter()
@@ -86,10 +88,17 @@ if __name__ == "__main__":
     rootdir = cleanedxml_path
     xmlpath_list = [join(rootdir, xml) for xml in listdir(rootdir) if xml[-4:] == '.xml']
     # xmlpath_list = [join(cleanedxml_path, '=astro-ph0001424.xml')]
-
-    # print(count_headings(xmlpath_list).most_common(500))
-    # [('introduction', 3510), ('conclusions', 893), ('conclusion', 541), ('acknowledgments', 528), ('discussion', 503), ('acknowledgements', 438), ('results', 343), ('summary', 257), ('concluding remarks', 123), ('preliminaries', 117), ('summary and conclusions', 92), ('appendix', 87), ('observations', 83), ('results and discussion', 80), ('discussion and conclusions', 79), ('acknowledgement', 73), ('numerical results', 65), ('the model', 65), ('proof of theorem', 60), ('acknowledgment', 59), ('references', 55), ('observations and data reduction', 55), ('summary and discussion', 54), ('', 45), ('related work', 39), ('examples', 38), ('applications', 35), ('introduction.', 32), ('model', 30), ('methods', 29), ('discussion and conclusion', 29), ('figure captions', 28)
+    heading_freqlist = count_headings(xmlpath_list)
     
+    # print(heading_freqlist.most_common(50))
+    print(sum(freq for title,freq in heading_freqlist.most_common() if freq > 1))
+    # [('introduction', 3510), ('conclusions', 893), ('conclusion', 541), ('acknowledgments', 528), ('discussion', 503), ('acknowledgements', 438), ('results', 343), ('summary', 257), ('concluding remarks', 123), ('preliminaries', 117), ('summary and conclusions', 92), ('appendix', 87), ('observations', 83), ('results and discussion', 80), ('discussion and conclusions', 79), ('acknowledgement', 73), ('numerical results', 65), ('the model', 65), ('proof of theorem', 60), ('acknowledgment', 59), ('references', 55), ('observations and data reduction', 55), ('summary and discussion', 54), ('', 45), ('related work', 39), ('examples', 38), ('applications', 35), ('introduction.', 32), ('model', 30), ('methods', 29), ('discussion and conclusion', 29), ('figure captions', 28)
+    # Total sections: 22746
+    # # of titles of which freq>1: 570
+    # Total freq of titles that shows more than once: 10605 (0.46623582168293326%)
+
+
+
     # show_cates_per_art()
     # Counter({'1': 4452, '2': 738, '3': 266, '4': 76, '5': 19, '6': 7, '7': 1})
     # 4452 articles associated with 1 categories: 0.8009 
@@ -100,5 +109,5 @@ if __name__ == "__main__":
     # 7 articles associated with 6 categories: 0.0013 
     # 1 articles associated with 7 categories: 0.0002
 
-    # show_secnum_per_art()
-    show_sectitle_per_art()
+    # show_seccount_per_art()
+    # show_sectitle_per_art()
