a932f1a3f697cf88b180b4bc5d18a852fb8c7232
karoteeni110 <akb48auy@sohu.com>
2019-08-06 11:55:44 +0300

    cates_per_art()

--- src/headingStats.py
+++ src/headingStats.py
@@ -56,13 +56,30 @@ def count_headings(xmlpath_list):
     cter = Counter(all_headings)
     return cter
 
-def show_heading_stats():
-    heading_cter = count_headings(xmlpath_list)
+def show_cates_per_art():
+    cter = Counter(str(count_cates(xml)) for xml in xmlpath_list)
+    print(cter)
+    for count in cter:
+        print('%s articles associated with %s categories: %.4f ' % (cter[count], count, (cter[count]/len(xmlpath_list))))
+
+def show_sec_per_art():
+    
 
 if __name__ == "__main__":
     VERBOSE, REPORT_EVERY = True, 100
     rootdir = cleanedxml_path
     xmlpath_list = [join(rootdir, xml) for xml in listdir(rootdir) if xml[-4:] == '.xml']
-    # xmlpath_list = ['/home/local/yzan/Desktop/Cleanskin/results/cleaned_xml/=astro-ph0001424.xml']
-    print(count_headings(xmlpath_list).most_common(80))
+    # xmlpath_list = [join(cleanedxml_path, '=astro-ph0001424.xml')]
+
+    # print(count_headings(xmlpath_list).most_common(80))
     # [('introduction', 3510), ('conclusions', 893), ('conclusion', 541), ('acknowledgments', 528), ('discussion', 503), ('acknowledgements', 438), ('results', 343), ('summary', 257), ('concluding remarks', 123), ('preliminaries', 117), ('summary and conclusions', 92), ('appendix', 87), ('observations', 83), ('results and discussion', 80), ('discussion and conclusions', 79), ('acknowledgement', 73), ('numerical results', 65), ('the model', 65), ('proof of theorem', 60), ('acknowledgment', 59), ('references', 55), ('observations and data reduction', 55), ('summary and discussion', 54), ('', 45), ('related work', 39), ('examples', 38), ('applications', 35), ('introduction.', 32), ('model', 30), ('methods', 29), ('discussion and conclusion', 29), ('figure captions', 28)
+    
+    # show_cates_per_art()
+    # Counter({'1': 4452, '2': 738, '3': 266, '4': 76, '5': 19, '6': 7, '7': 1})
+    # 4452 articles associated with 1 categories: 0.8009 
+    # 738 articles associated with 2 categories: 0.1328 
+    # 76 articles associated with 4 categories: 0.0137 
+    # 266 articles associated with 3 categories: 0.0479 
+    # 19 articles associated with 5 categories: 0.0034 
+    # 7 articles associated with 6 categories: 0.0013 
+    # 1 articles associated with 7 categories: 0.0002
