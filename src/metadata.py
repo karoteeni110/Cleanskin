b0fa9c93013e0c26167297d5a6ad975201607431
karoteeni110 <akb48auy@sohu.com>
2019-08-07 12:26:10 +0300

    add title

--- src/metadata.py
+++ src/metadata.py
@@ -11,23 +11,32 @@ def url2artid(url):
     else:
         return urlid[0] + urlid[1] # 0001, 0002
 
+def elem2artid(artelem):
+    return url2artid(artelem[5].text)
+
 def demath(abstract):
     return re.sub(r'\$.*?\$', ' ', abstract, flags=re.M | re.S) # multi-line | dot-match-all
+
+def meta_to_add(artelem):
+    title = artelem[1].text
+    abstract = demath(artelem[3].text)
+    categories = artelem[-1].text
+    author = artelem[2].text
+    return title, abstract, categories, author
 
 def read_metaxml(meta_xmlpath):
     root = ET.parse(meta_xmlpath).getroot()
     id2meta = {}
     for article in root:
-        artid = url2artid(article[5].text)
-        abstract, cates = demath(article[3].text), article[-1].text
-        author = article[2].text
-        id2meta[artid] = {'abstract': abstract, 'categories': cates, 'author': author}
+        artid = elem2artid(article)
+        title, abstract, categories, author = meta_to_add(article)
+        id2meta[artid] = {'abstract': abstract, 'categories': categories, 'author': author, 'title': title}
     return id2meta
 
 def get_urlid2meta(metadirpath = metadatas_path):
     id2meta = {}
     begin = time.time()
-    print('Reading metadata. It takes about 1 min')
+    print('Reading metadata... It takes about 1 min')
     for metaxml in listdir(metadirpath): # ['Astrophysics.xml']:
         if metaxml[-3:] == 'xml':
             print('Reading', metaxml, '...')
