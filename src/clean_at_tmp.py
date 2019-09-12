5f52f6e75eab5885f04292b554d955797c8195a0
karoteeni110 <akb48auy@sohu.com>
2019-09-12 16:08:44 +0300

    pipeline

--- src/clean_at_tmp.py
+++ src/clean_at_tmp.py
@@ -1,8 +1,17 @@
 from newCleaner import *
-from subprocess import run, PIPE
+from subprocess import run, PIPE, CalledProcessError
 from shutil import copyfile, copytree, rmtree
 from os import listdir, remove
 from os.path import join
+
+def run_and_report(msg, cmd):
+    try:
+        run(cmd, shell=True, stderr=PIPE, check=True)
+    except CalledProcessError as e:
+        return e.stderr.decode('utf-8')
+    else: 
+        print(msg)
+
 
 def cp_1tar(tar_fn):
     src_tarpath = join('/cs/group/grp-glowacka/arxiv/xml', tar_fn)
@@ -11,7 +20,7 @@ def cp_1tar(tar_fn):
 
 def unzip_1tar(tar_fn):
     cmd = 'tar -xzf %s' % join('/tmp/arxiv', tar_fn)
-    run(cmd)
+    
 
 def rm_oldtar(tar_fn):
     remove(join('/tmp/arxiv', tar_fn))
@@ -30,6 +39,9 @@ def tarback(tar_fn):
 def rm_cleansed_dir(tar_fn):
     pass
 
+def mv_newtar(tar_fn):
+    pass
+
 def main(tar_fn):
     cp_1tar(tar_fn)
     unzip_1tar(tar_fn)
@@ -37,6 +49,7 @@ def main(tar_fn):
     cleanse(tar_fn)
     tarback(tar_fn)
     rm_cleansed_dir(tar_fn)
+    mv_newtar(tar_fn)
 
 if __name__ == "__main__":
     tarlist = [join('/cs/group/grp-glowacka/arxiv/xml', '0001.tar.gz')]
