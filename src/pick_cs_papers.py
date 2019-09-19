23d7af9f36cc8cef56a6599e08afff814d5e33bb
karoteeni110 <akb48auy@sohu.com>
2019-09-19 19:53:31 +0300

    framework of mallet

--- src/pick_cs_papers.py
+++ src/pick_cs_papers.py
@@ -1,5 +1,8 @@
 import xml.etree.ElementTree as ET
 from newCleaner import get_root
+from clean_at_tmp import level, format, run_and_report_done
+from os import listdir
+from paths import 
 
 def is_longtoken_sec(sec):
     """Check if the tokens within `sec` are concatenated:
@@ -8,7 +11,32 @@ def is_longtoken_sec(sec):
     """
     return False
 
+def cp_1tar(tarfn):
+    pass
+
+def unzip_1tar(tarfn):
+    pass
+
+def rm_oldtar(tarfn):
+    pass
+
+def pick_cs_papers(tarfn):
+    fulltext_dst = 
+    pass
+
+def rm_picked_dir(tarfn):
+    pass
+
+def main(tar_fn):
+    cp_1tar(tar_fn)
+    unzip_1tar(tar_fn)
+    rm_oldtar(tar_fn)
+    pick_cs_papers(tar_fn)
+    rm_picked_dir(tar_fn)
+    get_topic_probs() 
+    # Finally get `cs_ft_composition.txt`, `cs_abt_composition.txt`, `cs_ft_keys.txt`
 
 if __name__ == "__main__":
-    pass
+    tarlist = [fn for fn in listdir('/cs/group/grp-glowacka/arxiv/xml') \
+                if fn[-2:] == 'gz' and fn not in listdir('/cs/group/grp-glowacka/arxiv/cleaned_xml')] 
 
--- src/todolist.md
+++ src/todolist.md
@@ -1,2 +1,18 @@
-1. cs0303019
-Sections are kind of garbled.
+## cs0303019
+Sections are garbled.
+
+##  Mallet usage
+
+```
+./bin/mallet import-dir --input ../Cleanskin/data/cs_fulltext --output cs_fulltext.mallet --keep-sequence --remove-stopwords
+
+./bin/mallet import-dir --input ../Cleanskin/data/cs_abstract --output cs_abstract.mallet --keep-sequence --remove-stopwords
+
+./bin/mallet train-topics --input cs_fulltext.mallet --num-topics 100 --output-model cs_ftmodel.mallet --output-doc-topics cs_ft_composition.txt --output-topic-keys cs_ft_keys.txt --inferencer-filename cs_ft_inferencer.mallet
+
+./bin/mallet infer-topics --inferencer cs_ft_inferencer.mallet --input cs_abstract.mallet --output-doc-topics cs_abt_composition.txt
+```
+
+(Keyword .txt files (`XXX_keys.txt`) are just for checking if model is working well.)
+
+Distributions: cs_XX_composition.txt
