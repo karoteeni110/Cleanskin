c9a460efb6db4a0a3a6c994052d7443a485ee581
karoteeni110 <akb48auy@sohu.com>
2019-09-19 15:20:15 +0300

    check longtoken sec

---
+++ src/pick_cs_papers.py
@@ -0,0 +1,14 @@
+import xml.etree.ElementTree as ET
+from newCleaner import get_root
+
+def is_longtoken_sec(sec):
+    """Check if the tokens within `sec` are concatenated:
+    If len(text)/len(text.split()) > 10.0, return True ;
+    else: False
+    """
+    return False
+
+
+if __name__ == "__main__":
+    pass
+
