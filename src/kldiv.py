357604bf6dc4e545341cc27077c8cebf8485f0f7
karoteeni110 <akb48auy@sohu.com>
2019-10-07 16:54:06 +0300

    check if pids aligned

--- src/kldiv.py
+++ src/kldiv.py
@@ -7,11 +7,10 @@ def read_data(datafile):
     pd.set_option('precision', 21)
     df = pd.read_csv(datafile, sep="\t", header=None, float_precision='high')
     df[1] = df[1].apply(lambda x:x.split('/')[-1][:-4]) # strip file extension
-    print(df.head(3))
-    # return df
+    # print(df.head(3))
+    return df.head(3)
 
-def align_dfs(df1,df2):
-    return 0
+
 
 def merge_frames(df1, df2):
     return 0
@@ -19,14 +18,18 @@ def merge_frames(df1, df2):
 def get_div_dfs(fulltext_df, sec_df):
     """ 
     """
-    align_dfs(fulltext_df, sec_df)
-    p_i = fulltext_df.iloc[:,2:] 
-    q_i = sec_df.iloc[:, 2:]
-    kl_divs = np.multiply(p_i, np.log2(p_i)-np.log2(q_i))
-    cate_vec = 0
-    pid_with_cate = merge_frames(fulltext_df.iloc[:, :3], cate_vec)
-    div_df = merge_frames(pid_with_cate, kl_divs)
-    return div_df
+    if fulltext_df.loc[:,1].equals(sec_df.loc[:1]): # pids must be aligned
+        print('Aligned!')
+        # p_i = fulltext_df.iloc[:,2:] 
+        # q_i = sec_df.iloc[:, 2:]
+        # kl_divs = np.multiply(p_i, np.log2(p_i)-np.log2(q_i))
+        # cate_vec = 0
+        # pid_with_cate = merge_frames(fulltext_df.iloc[:, :3], cate_vec)
+        # div_df = merge_frames(pid_with_cate, kl_divs)
+        # return div_df
+    else:
+        print('DFs not aligned')
+        exit(0)
 
 def show_errbar():
     # plt.style.use('seaborn-whitegrid')
@@ -57,6 +60,10 @@ def ytick():
 if __name__ == "__main__":
     # ytick()
     # show_errbar()
-    read_data('./KLdiv/cs_ft_composition.txt')
-    # ft_df, abt_df = read_data('./KLdiv/cs_ft_composition.txt'), read_data('./KLdiv/cs_abs_composition.txt')
-    # kl_df = get_kldf(ft_df, abt_df)
+    ft_df = read_data('./KLdiv/cs_ft_composition.txt')
+    
+    abt_df = read_data('./KLdiv/cs_abt_composition.txt')
+    # print(ft_df)
+    # print()
+    # print(abt_df)
+    get_div_dfs(ft_df, abt_df)
