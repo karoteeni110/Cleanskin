1235d9b8c3a74ce109d255d3514cb1a43f4d5b00
karoteeni110 <akb48auy@sohu.com>
2019-10-30 14:35:41 +0200

    docstring

--- src/paths.py
+++ src/paths.py
@@ -25,6 +25,8 @@ metadatas_path = join(data_path, 'arxiv_metadata')
 cs_lda_dir = join(results_path, 'cs_lda')
 fulltext_dir = join(cs_lda_dir, 'fulltext')
 kldiv_dir = join(src_path, 'KLdiv')
+perpsets_testdir = join(results_path, 'perpsets/10test')
+perpsets_traindir = join(results_path, 'perpsets/90train')
 
 if __name__ == "__main__":
     # Check that we are reading the correct file
--- src/splitData.py
+++ src/splitData.py
@@ -1,18 +1,27 @@
 from kldiv import get_pid2meta
-from paths import fulltext_dir, metadatas_path
+from paths import fulltext_dir, metadatas_path, perpsets_testdir, perpsets_traindir
 from shutil import copy
 from os import listdir
 from random import choice, shuffle
 from collections import Counter
+from os.path import join 
 import pandas as pd
 
 def mk_cate_dirs(dst=''):
     return 0
 
 def get_pid2dst(metaxml_list=listdir(metadatas_path)):
+    """Returns: pid2cates -- dictionary 
+                            key: pid
+                            value: CS subcategory acronym
+                cate2pcount -- pd series
+                            index: CS subcate acronym
+                            value: count of papers in the subcate
+    """
     pid2meta = get_pid2meta(metaxml_list)
     pid2cates = dict()
     cate2pcount = Counter()
+    print('Collecting paper categories...')
     for pid in pid2meta:
         pcate = choice([c[3:] for c in pid2meta[pid]['categories'].split(', ') 
                             if c[:2]=='cs'])
@@ -25,18 +34,23 @@ def get_pid2dst(metaxml_list=listdir(metadatas_path)):
     cate2pcount = pd.Series(cate2pcount)
     return pid2cates, cate2pcount
 
-def copy_to_catedir(pid, cate):
-    src = ''
-    dst = ''
-    copy(src, dst)
+def copy_to_catedir(paperfn, cate, to_trainset):
+    src = join(PAPERDIR, paperfn)
+    if to_trainset:
+        dst = join(perpsets_traindir, paperfn)
+    else:
+        dst = join(perpsets_testdir, paperfn)
+    # copy(src, dst)
     
 
 
 if __name__ == "__main__":
     catedir_paths = mk_cate_dirs()
-    cs_paper_pids = [i[:-4] for i in listdir(fulltext_dir) if i[:-3]=='txt']
-    pid2cate, cate2pcount = get_pid2dst()
+    cs_paper_fns = listdir(fulltext_dir) # [i[:-4] for i in listdir(fulltext_dir) if i[-3:]=='txt']
+    pid2cate, cate2pcount = get_pid2dst(['Computer_Science.xml'])
     
-    cs_paper_pids = shuffle(cs_paper_pids)
-    for pid in cs_paper_pids:
-        copy_to_catedir(pid, pid2cate[pid])
+    cs_paper_fns = shuffle(cs_paper_fns)
+    PAPERDIR = fulltext_dir
+    for paperfn in cs_paper_fns:
+        pid = paperfn[:-4] # strip '.txt'
+        copy_to_catedir(paperfn, pid2cate[pid])
