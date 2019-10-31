f89b1833d68209a9c376b182bad50838b5dccdda
karoteeni110 <akb48auy@sohu.com>
2019-10-31 16:18:46 +0200

    go

--- src/splitData.py
+++ src/splitData.py
@@ -11,7 +11,7 @@ import numpy as np
 def mk_cate_dirs(dst=''):
     return 0
 
-def get_pid2dst(metaxml_list=listdir(metadatas_path)):
+def get_pid2dst(metaxml_list=listdir(metadatas_path), fulltext_dir_fns):
     """Returns: pid2cates -- dictionary 
                             key: pid
                             value: CS subcategory acronym
@@ -20,10 +20,15 @@ def get_pid2dst(metaxml_list=listdir(metadatas_path)):
                             value: count of papers in the subcate
     """
     pid2cate= pd.Series(get_pid2meta(metaxml_list))
+    fulltext_dir_pids = pd.Series(fulltext_dir_fns).apply(lambda x: x[:-4]) # strip '.txt'
+
     print('Collecting categories...')
     pid2cate = pid2cate.apply(lambda x: x.get('categories').split(', '))
     pid2cate = pid2cate.apply(lambda cates: [c[3:] for c in cates if c[:2]=='cs'] )
     pid2cate = pid2cate.apply(lambda subcates: choice(subcates))
+
+    # pid2cate = pid2cate.apply(lambda x: x if x.index )
+
     cate2pcount = dict()
     for subcate in set(pid2cate):
         cate2pcount[subcate] = len(pid2cate[pid2cate==subcate])
@@ -35,45 +40,54 @@ def get_pid2dst(metaxml_list=listdir(metadatas_path)):
     cate2pcount = pd.Series(cate2pcount)
     return pid2cate, cate2pcount
 
-def copy_to_catedir(paperfn, cate, cate2test_to_add):
+def copy_to_catedir(paperfn, cate, test_to_add):
     src = join(PAPERDIR, paperfn)
-    to_testset = cate2test_to_add.at[cate]
-    if (cate2pcount.at[cate]-to_testset)%50 ==0:
-        print('Cate:',cate, 'to add:', cate2test_to_add.at[cate])
-    if to_testset>0:
+    if test_to_add > 0:
         dst = join(perpsets_testdir, paperfn)
-        cate2test_to_add.at[cate] -= 1    
+        gototest = 1
     else:
         dst = join(perpsets_traindir, paperfn)
+        gototest = 0
     copy(src, dst)
-    
-    if cate2test_to_add.at[cate] == 0:
-        print(cate, 'training set collection done. %s/%s' % 
-                        (len(cate2test_to_add[cate2test_to_add==0]), len(cate2test_to_add)))
-    finished = 100-(cate2test_to_add.at[cate]/int(cate2pcount.at[cate]*0.1))*100
-    if finished % 5 == 0 : # Report every 5 percent
-        print('Cate:', cate+'('+acro2cate.at[cate]+')', 'process:', finished + '%%;',
-                 'to add:', cate2test_to_add.at[cate])
-    
-
+    return gototest
 
 if __name__ == "__main__":
-    catedir_paths = mk_cate_dirs()
+    # catedir_paths = mk_cate_dirs()
     print('Listdir:', fulltext_dir, '...')
     cs_paper_fns = listdir(fulltext_dir) # [i[:-4] for i in listdir(fulltext_dir) if i[-3:]=='txt']
     shuffle(cs_paper_fns)
     print('... Listdir done.')
-    pid2cate, cate2pcount = get_pid2dst(['Computer_Science.xml'])
-    acro2cate = get_acro2cate_dict()
+    pid2cate, cate2pcount = get_pid2dst(['Computer_Science.xml'], cs_paper_fns)
+    # acro2cate = get_acro2cate_dict()
+
+    totest, totrain = pd.Series(), pd.Series()
+    for cate in set(pid2cate):
+        papers_in_cate = pid2cate[pid2cate==cate]
+        sumcount = len(papers_in_cate)
+        boundary = int(sumcount*0.1)
+        totest = totest.append(papers_in_cate[:boundary].index.to_series())
+        totrain = totrain.append(papers_in_cate[boundary:].index.to_series())
 
     cate2test_toadd = pd.Series(cate2pcount*0.1, dtype='int')
+    sumkeeper = cate2test_toadd.copy()
     PAPERDIR =  fulltext_dir # join(results_path, 'test')
     for paperfn in cs_paper_fns:
         pid = paperfn[:-4] # strip '.txt'
         try:
             cate = pid2cate.pop(pid)
-            copy_to_catedir(paperfn, cate, cate2test_toadd)
-            print('OLD:', cate, cate2test_toadd[cate])
-            print('NEW:', cate, cate2test_toadd[cate])
+            
+            gototest = copy_to_catedir(paperfn, cate, cate2test_toadd.at[cate])
+            if gototest:
+                cate2test_toadd[cate] -=1
+
+            # if old.equals(cate2test_toadd):
+            #     print('counter does not change')
+            #     exit(0)
+
+            done = sumkeeper.at[cate]-cate2test_toadd.at[cate]
+            if done % 50 == 0:
+                print('Test set for', cate+':', 
+                        done, '/', sumkeeper.at[cate], str(done*100/sumkeeper.at[cate])+'%%')
+
         except KeyError:
             continue
