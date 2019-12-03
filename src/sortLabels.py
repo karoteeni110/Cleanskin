641c3285fdb3560eb8bc020e88ade7c7d7f3f6ce
karoteeni110 <akb48auy@sohu.com>
2019-12-03 18:08:46 +0200

    has_noneng

--- src/sortLabels.py
+++ src/sortLabels.py
@@ -1,11 +1,18 @@
 import pandas as pd
 from paths import data_path, results_path
 from os.path import join
+from collections import Counter
+import nltk,re
 
 def read_one_result(result_txt_path):
     with open(result_txt_path, 'r') as f:
         df = pd.read_csv(f, sep=" ", index_col='heading',names=['heading','label'], dtype=str)
-    return df # dict(zip(df.heading, df.label)) 
+    return df  
+
+def has_noneng(string):
+    if re.search(r'[^a-z0-9._:&\-\(\),]+',string):
+        return True 
+    return False
 
 def merge_to_three_cols(df1, df2, df3):
     """
@@ -29,11 +36,17 @@ def merge_to_three_cols(df1, df2, df3):
     x = pd.merge(df1,df2,on='heading',how='inner')
     x = pd.merge(x,df3,on='heading',how='inner')
     x.label = x.label.str.cat([x.label_x,x.label_y],sep=',')
-    x = x.drop(['label_x','label_y'],axis=1)
-    x.label = 
+    x = x.drop(['label_x','label_y'], axis=1)\
+        .apply(lambda o:o.str.split(','))\
+        ['label'].apply(pd.Series)
+    # x = x.label.apply(pd.Series)
+    x = x.apply(lambda o:Counter(o), axis=1)\
+        .apply(lambda m:[i for i in m if m[i]>=2 and type(i)==str and not has_noneng(i)])\
+        .to_dict() # Split label
     return x
 
 if __name__ == "__main__":
     ay_path, al_path, do_path = join(data_path, 'AY_RESULTS.txt'), join(data_path, 'ALAN_RESULTS.txt'), join(data_path, 'DOROTA_RESULTS.txt')
     ay_df, al_df, do_df = read_one_result(ay_path), read_one_result(al_path), read_one_result(do_path)
+    final = merge_to_three_cols(ay_df, al_df, do_df)
     print()
