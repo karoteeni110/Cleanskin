6f30faaf9c180a4ee5dd3ff49b2b8bc59ec7e45a
karoteeni110 <akb48auy@sohu.com>
2020-01-14 19:46:21 +0200

    classif_with_ft done

--- src/eval_by_clsf.py
+++ src/eval_by_clsf.py
@@ -1,30 +1,44 @@
 """Evaluate topic models with multilabel classification"""
+from kldiv import read_data, get_pid2cate_dict
 
-from sklearn.datasets import load_iris
+from sklearn.datasets import make_multilabel_classification
+from sklearn.multiclass import OneVsRestClassifier
+from sklearn.svm import SVC
 from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import MultiLabelBinarizer
+from sklearn.metrics import accuracy_score
 import numpy as np
 import pandas as pd
 import itertools
-from kldiv import read_data, get_pid2cate_dict
+
 
 def example():
-    X, y = datasets.load_iris(return_X_y=True)
-    # >>> OneVsOneClassifier(LinearSVC(random_state=0)).fit(X, y).predict(X)
+    X, Y = make_multilabel_classification(n_classes=2, n_labels=1,
+                                      allow_unlabeled=True,
+                                      random_state=1)
+    classif = OneVsRestClassifier(SVC(kernel='linear'))
+    pred = classif.fit(X, Y).predict(X)
 
-def docdf2y(docdf):
-    """https://scikit-learn.org/stable/modules/multiclass.html#multilabel-classification-format"""
+def df2xy(df):
     catedict = get_pid2cate_dict(metaxml_list=['Computer_Science.xml'])
-    cate_series = docdf.pid.map(catedict).to_list()
-    # lb2idx = {cate:idx for idx,cate in enumerate(cates)}
-    y = MultiLabelBinarizer().fit(catedict.values())
-    return y
+    df.pid = df.pid.map(catedict)
+    df = df.dropna(subset=['pid'])
+    df.pid = df.pid.apply(lambda x: tuple(x))
+    X = df.iloc[:,1:].to_numpy()
+    y = MultiLabelBinarizer().fit_transform(df.pid)
+    return X, y, y.classes_
 
 def cls_with_ft(ft_comp_df):
-    X = ft_comp_df.iloc[:,1:].to_numpy()
-    y = pid2y(doc_df.loc[:,'pid'])
+    X, y, y_lbs = df2xy(ft_comp_df.iloc[:1000,:])
     X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.1, random_state=0)
-    
+    classif = OneVsRestClassifier(SVC(kernel='linear')).fit(X_train, y_train)
+    # pred = classif.predict(X_test)
+    # acc = classif.score(X_test,y_test)
+
+    for cate in y_lbs:
+        pred = classif.predict(X_test)
+        cate_acc = accuracy_score(y_test, pred)
+        print(cate, cate_acc)
 
 if __name__ == "__main__":
     abst_comp_path = '/home/yzan/Desktop/mallet-2.0.8/cs_abstract_comp.txt'
