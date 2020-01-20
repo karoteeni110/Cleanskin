fe3b0f41d36690827c4ef857c8b5ef43b4dbe18e
karoteeni110 <akb48auy@sohu.com>
2020-01-20 15:37:02 +0200

    separate test

--- src/eval_by_clsf.py
+++ src/eval_by_clsf.py
@@ -20,31 +20,31 @@ def example():
     classif = OneVsOneClassifier(SVC(kernel='linear'))
     pred = classif.fit(X, Y).predict(X)
 
-def df2xy(train_df, test_df):
+def traindf2xy(train_df):
     """"""
-    mlb, le = MultiLabelBinarizer(), LabelEncoder()
-    train_df, test_df = align_dfs(train_df, test_df)
-    for xdf in [0,1]:
-        df = [train_df, test_df][xdf]
-        df.pid = df.pid.map(CATEDICT)
-        df = df.dropna(subset=['pid']).sample(frac=1) # Drop those categories and shuffle
-        df.loc[:,'pid'] = df.pid.apply(tuple)
-        df.loc[:,'pid'] = df.pid.apply(sorted).apply(lambda x: ','.join(str(i) for i in x)) # list to str 
-        if xdf == 0: # if it is train df
-            X_train = df.iloc[:,1:].to_numpy()
-            # y_train = mlb.fit_transform(df.pid) # [0,0,0,...,1] format labels
-            # y_train = np.apply_along_axis(lambda bilb:','.join(str(i) for i in bilb), 1, y_train) # '010101' format labels
-            y_train = le.fit_transform(df.pid)
-            mlb = mlb.fit(df.pid)
-        else: # test df
-            X_test = df.iloc[:,1:].to_numpy()
-            # y_test = mlb.transform(df.pid) # DIFFERENT FROM fit_transform!!!
-            # y_test = np.apply_along_axis(lambda bilb:''.join(str(i) for i in bilb))
-            y_test = le.transform(df.pid)
-    return X_train, X_test, y_train, y_test, le.classes_, mlb.classes_
+    mlb, le = MultiLabelBinarizer(), LabelEncoder()    
+    df = train_df
+    df.pid = df.pid.map(CATEDICT)
+    df = df.dropna(subset=['pid']).sample(frac=1) # Drop those categories and shuffle
+    df.loc[:,'pid'] = df.pid.apply(tuple)
+    df.loc[:,'pid'] = df.pid.apply(sorted).apply(lambda x: ','.join(str(i) for i in x)) # list to str 
+    X_train = df.iloc[:,1:].to_numpy()
+        # y_train = mlb.fit_transform(df.pid) # [0,0,0,...,1] format labels
+        # y_train = np.apply_along_axis(lambda bilb:','.join(str(i) for i in bilb), 1, y_train) # '010101' format labels
+    y_train = le.fit_transform(df.pid)
+        # X_test = df.iloc[:,1:].to_numpy()
+        # # y_test = mlb.transform(df.pid) # DIFFERENT FROM fit_transform!!!
+        # # y_test = np.apply_along_axis(lambda bilb:''.join(str(i) for i in bilb))
+        # y_test = le.transform(df.pid)
+    return X_train, y_train, le
+
+def testdf2xy(test_df, cate, lbler):
+    X_test, y_test = 0, 0
+    cate_df = test_df[test_df]
+    return X_test, y_test
 
 def cls_with_ft(train_df, test_df):
-    X_train, X_test, y_train, y_test, y_le_labels, y_mlb_labels = df2xy(train_df, test_df)
+    X_train, y_train, le = traindf2xy(train_df)
     print('Training...')
     classif = OneVsOneClassifier(SVC(kernel='linear')).fit(X_train, y_train)
     
