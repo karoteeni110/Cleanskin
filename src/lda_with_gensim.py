d6d296a3302c9da75a8ed7a8ec19400d93771d55
karoteeni110 <akb48auy@sohu.com>
2020-02-04 17:51:01 +0200

    lda_with_gensim

---
+++ src/lda_with_gensim.py
@@ -0,0 +1,141 @@
+#import logging
+#logging.basicConfig(format='%(asctime)s : %(levelname)s : %(message)s', level=logging.INFO)
+
+from sys import exit, argv, stderr
+import os.path
+import re
+
+from nltk.tokenize import RegexpTokenizer
+from gensim.corpora import Dictionary
+from gensim.models import LdaModel
+
+
+def extract_documents(fname, whitelist) :
+    pids = []
+    docs = []
+    with open(fname) as f :
+        for line in f :
+            line = line.strip()
+            if not line : 
+                continue
+            line = line.encode('utf-8', errors='replace').decode()
+            id,line = line.split(" ", 1)
+
+            if id not in whitelist :
+                continue
+
+            pids.append(id)
+            docs.append(line)
+            #if len(docs) == 100 :
+            #    break
+    return pids,docs
+
+if len(argv) != 6 :
+    print("Usage: {} <fulltext_dir> <abstract_dir> <dst_dir> <#topics> <seed>\n".format(argv[0]))
+    exit(1)
+
+ft_dir = argv[1]
+ab_dir = argv[2]
+results_dir = argv[3]
+num_topics = [ int(i) for i in argv[4].split(',')] #int(argv[4])
+random_seed = int(argv[5])
+
+if not os.path.exists(results_dir) :
+    print("{} does not exist, exiting...\n".format(results_dir), file=stderr)
+    exit(1)
+
+whitelist = set()
+with open('chiir_whitelist_300.txt') as f :
+    for line in f :
+        whitelist.add(line.strip())
+
+ids,docs = extract_documents(ft_dir, whitelist)
+ab_ids,ab_docs = extract_documents(ab_dir, whitelist)
+print("read", len(docs),"documents")
+
+
+# print("tokenizing...")
+# docs = tokenize_filter(docs)
+# ab_docs = tokenize_filter(ab_docs)
+
+
+print("building dictionary...")
+dictionary = Dictionary(docs)
+print('Number of unique tokens: %d' % len(dictionary))
+corpus = [dictionary.doc2bow(doc) for doc in docs]
+ab_corpus = [dictionary.doc2bow(doc) for doc in ab_docs]
+
+
+# Train LDA model.
+chunksize = 2000
+passes = 10      # num epoches
+iterations = 50   # max passes of E-step
+eval_every = None
+
+# Make a index to word dictionary.
+temp = dictionary[0]  # This is only to "load" the dictionary.
+id2word = dictionary.id2token
+
+
+
+overview = open(results_dir + '/overview.txt', 'w')
+print("topics", "coherence", file=overview)
+
+for t in num_topics :
+
+    print("topics = {}".format(t))
+
+    model = LdaModel(
+        corpus=corpus,
+        id2word=id2word,
+        chunksize=chunksize,
+        alpha='auto',
+        eta='auto',
+        iterations=iterations,
+        num_topics=t,
+        passes=passes,
+        eval_every=eval_every,
+        random_state=random_seed
+    )
+
+    top_topics = model.top_topics(corpus, docs, dictionary, coherence='c_v', topn=20, processes=4)
+
+
+    # Average topic coherence 
+    avg_topic_coherence = sum([t[1] for t in top_topics]) / t
+    print(t, avg_topic_coherence, file=overview)
+
+    with open(results_dir + '/%d_%d_stats.txt' % (t, random_seed), 'w') as f :
+        print('Average topic coherence: %.4f.' % avg_topic_coherence, file=f)
+
+    with open(results_dir + '/%d_%d_topics.txt' % (t, random_seed), 'w') as f :
+        for ind,top in enumerate(top_topics) :
+            print("%d %.4f %s" % (ind, top[1], ','.join([ w for p,w in top[0] ])), file=f)
+
+    topic_dist = model.get_document_topics(corpus)
+    with open(results_dir + '/%d_%d_fulltext_composition_compact.txt' % (t, random_seed), 'w') as f :
+        for id,d in zip(ids, topic_dist) :
+            print(id, ' '.join(["%d=%f" % (tid,prob) for tid,prob in d ]), file=f)
+
+    with open(results_dir + '/%d_%d_fulltext_composition.txt' % (t, random_seed), 'w') as f :
+        for id,d in zip(ids, topic_dist) :
+            remainder = 0.0 if (len(d) == t) else (1.0 - sum([ prob for _,prob in d ])) / float(t - len(d))
+            tmp = dict(d)
+            print(id, ' '.join([str(tmp.get(i, remainder)) for i in range(t)]), file=f)
+
+    topic_dist = model.get_document_topics(ab_corpus)
+    with open(results_dir + '/%d_%d_abstract_composition_compact.txt' % (t, random_seed), 'w') as f :
+        for id,d in zip(ids, topic_dist) :
+            print(id, ' '.join(["%d=%f" % (tid,prob) for tid,prob in d ]), file=f)
+
+    # this is always different
+    #print(topic_dist[6])
+
+    with open(results_dir + '/%d_%d_abstract_composition.txt' % (t, random_seed), 'w') as f :
+        for id,d in zip(ids, topic_dist) :
+            remainder = 0.0 if (len(d) == t) else (1.0 - sum([ prob for _,prob in d ])) / float(t - len(d))
+            tmp = dict(d)
+            print(id, ' '.join([str(tmp.get(i, remainder)) for i in range(t)]), file=f)
+
+overview.close()
+
